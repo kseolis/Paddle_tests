f9afd654d3bb1a5b798b3018f2514f049b757d64
vnikolaenko <vnikolaenko@web3tech.ru>
2022-04-15 13:22:04 +0300

    added Randomizer

---
+++ src/test/java/Randomizer.java
@@ -0,0 +1,18 @@
+import java.util.Random;
+
+public class Randomizer {
+
+    public static String randomNumAndLetterString (final long targetStringLength) {
+        int leftLimit = 48; // numeral '0'
+        int rightLimit = 122; // letter 'z'
+        Random random = new Random();
+
+        String generatedString = random.ints(leftLimit, rightLimit + 1)
+                .filter(i -> (i <= 57 || i >= 65) && (i <= 90 || i >= 97))
+                .limit(targetStringLength)
+                .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+                .toString();
+        System.out.println(generatedString);
+        return generatedString;
+    }
+}
