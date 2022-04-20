5c3a0ef9932deef357442cf84783a9e68b1c5d6d
vnikolaenko <vnikolaenko@web3tech.ru>
2022-04-20 11:18:31 +0300

    added async in @Before tests method for faster create tests data

--- src/test/java/im/mak/paddle/e2e/transactions/BurnTransactionTest.java
+++ src/test/java/im/mak/paddle/e2e/transactions/BurnTransactionTest.java
@@ -4,45 +4,48 @@ import com.wavesplatform.transactions.BurnTransaction;
 import com.wavesplatform.transactions.common.AssetId;
 import com.wavesplatform.wavesj.info.TransactionInfo;
 import im.mak.paddle.Account;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static com.wavesplatform.wavesj.ApplicationStatus.SUCCEEDED;
 import static im.mak.paddle.Node.node;
 import static im.mak.paddle.helpers.Randomizer.randomNumAndLetterString;
+import static im.mak.paddle.util.Async.async;
 import static im.mak.paddle.util.Constants.*;
 import static im.mak.paddle.util.Constants.MIN_FEE;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 public class BurnTransactionTest {
-    private Account marrie;
-    private long marrieWavesBalance;
-    private AssetId issuedAsset;
+    private static Account marrie;
+    private static long marrieWavesBalance;
+    private static AssetId issuedAsset;
 
-    @BeforeEach
-    void before() {
+    @BeforeAll
+    static void before() {
         marrie = new Account(DEFAULT_FAUCET);
-        marrie.createAlias(randomNumAndLetterString(15));
-        issuedAsset = marrie.issue(i -> i.name("Test_Asset").quantity(1000)).tx().assetId();
-        marrieWavesBalance = marrie.getBalance(AssetId.WAVES);
+        async(
+            () -> marrie.createAlias(randomNumAndLetterString(15)),
+            () -> issuedAsset = marrie.issue(i -> i.name("Test_Asset").quantity(1000)).tx().assetId()
+        );
     }
 
     @Test
     @DisplayName("test burn minimum quantity asset")
-    void reissueMinimumAssets() {
+    void burnMinimumAssets() {
         burnTransaction(ASSET_QUANTITY_MINIMUM);
     }
 
     @Test
     @DisplayName("test burn maximum quantity asset")
-    void reissueMaximumAssets() {
+    void burnMaximumAssets() {
         long burnSum = marrie.getAssetBalance(issuedAsset);
         burnTransaction(burnSum);
     }
 
     private void burnTransaction(long amount) {
+        marrieWavesBalance = marrie.getBalance(AssetId.WAVES);
         long balanceAfterBurn = marrie.getBalance(issuedAsset) - amount;
         BurnTransaction tx = marrie.burn(amount, issuedAsset).tx();
 
--- src/test/java/im/mak/paddle/e2e/transactions/ReissueTransactionTest.java
+++ src/test/java/im/mak/paddle/e2e/transactions/ReissueTransactionTest.java
@@ -4,55 +4,74 @@ import com.wavesplatform.transactions.ReissueTransaction;
 import com.wavesplatform.transactions.common.AssetId;
 import com.wavesplatform.wavesj.info.TransactionInfo;
 import im.mak.paddle.Account;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static com.wavesplatform.wavesj.ApplicationStatus.SUCCEEDED;
 import static im.mak.paddle.Node.node;
 import static im.mak.paddle.helpers.Randomizer.randomNumAndLetterString;
+import static im.mak.paddle.util.Async.async;
 import static im.mak.paddle.util.Constants.*;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 public class ReissueTransactionTest {
-    private Account marrie;
-    private long marrieWavesBalance;
-    private AssetId issuedAsset;
+    private static Account marrie;
+    private static long marrieWavesBalance;
+    private static AssetId issuedAssetId;
+    private static AssetId issuedSmartAssetId;
+    long feeForSmartAssetReissue = MIN_FEE + EXTRA_FEE;
 
-    @BeforeEach
-    void before() {
+    @BeforeAll
+    static void before() {
         marrie = new Account(DEFAULT_FAUCET);
-        marrie.createAlias(randomNumAndLetterString(15));
-        issuedAsset = marrie.issue(i -> i.name("Test_Asset").quantity(1000)).tx().assetId();
-        marrieWavesBalance = marrie.getBalance(AssetId.WAVES);
+        async(
+            () -> marrie.createAlias(randomNumAndLetterString(15)),
+            () -> issuedAssetId = marrie.issue(i -> i.name("Test_Asset").quantity(1000)).tx().assetId(),
+            () -> issuedSmartAssetId =
+                    marrie.issue(i -> i.name("Test_Asset").quantity(1000).script("2 * 2 == 4")).tx().assetId()
+        );
     }
 
     @Test
     @DisplayName("test reissue minimum quantity asset")
     void reissueMinimumAssets() {
-        reissueTransaction(ASSET_QUANTITY_MINIMUM);
+        reissueTransaction(ASSET_QUANTITY_MINIMUM, issuedAssetId, MIN_FEE);
     }
 
     @Test
     @DisplayName("test reissue maximum quantity asset")
-    void reissueMaximumAssets() {
-        long reissueSum = ASSET_QUANTITY_MAXIMUM - marrie.getAssetBalance(issuedAsset);
-        reissueTransaction(reissueSum);
+    void reissueAlmostMaximumAssets() {
+        long reissueSum = ASSET_QUANTITY_MAXIMUM - marrie.getAssetBalance(issuedAssetId) - ASSET_QUANTITY_MINIMUM;
+        reissueTransaction(reissueSum, issuedAssetId, MIN_FEE);
     }
 
-    private void reissueTransaction(long amount) {
-        long balanceAfterReissue = marrie.getBalance(issuedAsset) + amount;
-        ReissueTransaction tx = marrie.reissue(amount, issuedAsset, i -> i.reissuable(true)).tx();
+    @Test
+    @DisplayName("test reissue minimum quantity smart asset")
+    void reissueMinimumSmartAssets() {
+        reissueTransaction(ASSET_QUANTITY_MINIMUM, issuedSmartAssetId, feeForSmartAssetReissue);
+    }
+
+    @Test
+    @DisplayName("test reissue maximum quantity smart asset")
+    void reissueAlmostMaximumSmartAssets() {
+        long reissueSum = ASSET_QUANTITY_MAXIMUM - marrie.getAssetBalance(issuedSmartAssetId) - ASSET_QUANTITY_MINIMUM;
+        reissueTransaction(reissueSum, issuedSmartAssetId, feeForSmartAssetReissue);
+    }
+
+    private void reissueTransaction(long amount, AssetId assetId, long fee) {
+        marrieWavesBalance = marrie.getBalance(AssetId.WAVES);
+        long balanceAfterReissue = marrie.getBalance(assetId) + amount;
+        ReissueTransaction tx = marrie.reissue(amount, assetId, i -> i.reissuable(true)).tx();
 
         TransactionInfo txInfo = node().getTransactionInfo(tx.id());
 
         assertAll(
-                () -> assertThat(marrie.getAssetBalance(issuedAsset)).isEqualTo(balanceAfterReissue),
-                () -> assertThat(marrie.getWavesBalance()).isEqualTo(marrieWavesBalance - MIN_FEE),
+                () -> assertThat(marrie.getAssetBalance(assetId)).isEqualTo(balanceAfterReissue),
+                () -> assertThat(marrie.getWavesBalance()).isEqualTo(marrieWavesBalance - fee),
                 () -> assertThat(txInfo.applicationStatus()).isEqualTo(SUCCEEDED),
-                () -> assertThat((Object) txInfo.tx().fee().value()).isEqualTo(MIN_FEE)
+                () -> assertThat((Object) txInfo.tx().fee().value()).isEqualTo(fee)
         );
-        System.out.println("after balance " + balanceAfterReissue);
     }
 }
