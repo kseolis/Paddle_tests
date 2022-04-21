522c85ce656968e6e56b0cf7d2f85d70719a356e
vnikolaenko <vnikolaenko@web3tech.ru>
2022-04-21 14:44:15 +0300

    added CreateAliasTransactionTest.java Account.java - delete unused import

--- src/main/java/im/mak/paddle/Account.java
+++ src/main/java/im/mak/paddle/Account.java
@@ -15,7 +15,6 @@ import im.mak.paddle.token.Asset;
 import im.mak.paddle.token.Token;
 import im.mak.paddle.util.Script;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Consumer;
 import java.util.regex.Pattern;
--- src/test/java/im/mak/paddle/e2e/transactions/CreateAliasTransactionTest.java
+++ src/test/java/im/mak/paddle/e2e/transactions/CreateAliasTransactionTest.java
@@ -1,4 +1,56 @@
 package im.mak.paddle.e2e.transactions;
 
+import com.wavesplatform.transactions.CreateAliasTransaction;
+import com.wavesplatform.wavesj.info.TransactionInfo;
+import im.mak.paddle.Account;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static com.wavesplatform.wavesj.ApplicationStatus.SUCCEEDED;
+import static im.mak.paddle.Node.node;
+import static im.mak.paddle.helpers.Randomizer.randomNumAndLetterString;
+import static im.mak.paddle.util.Constants.*;
+import static im.mak.paddle.util.Constants.MIN_FEE;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
 public class CreateAliasTransactionTest {
+    private static Account alice;
+    private static String aliceAlias;
+
+    @BeforeAll
+    static void before() {
+        alice = new Account(DEFAULT_FAUCET);
+    }
+    @Test
+    @DisplayName("test create minimally short alias")
+    void createMinShortAlias() {
+        aliceAlias = randomNumAndLetterString(4);
+        createAliasTransaction(aliceAlias);
+    }
+
+    @Test
+    @DisplayName("test create maximum long alias")
+    void createMaxLongAlias() {
+        aliceAlias = randomNumAndLetterString(30);
+        createAliasTransaction(aliceAlias);
+    }
+
+    private void createAliasTransaction(String alias) {
+        long aliceWavesBalance = alice.getWavesBalance();
+        long balanceAfterCreateAlias = aliceWavesBalance - MIN_FEE;
+        CreateAliasTransaction tx = alice.createAlias(alias).tx();
+
+        TransactionInfo txInfo = node().getTransactionInfo(tx.id());
+
+        assertAll(
+                () -> assertThat(alice.getWavesBalance()).isEqualTo(balanceAfterCreateAlias),
+                () -> assertThat(txInfo.applicationStatus()).isEqualTo(SUCCEEDED),
+                () -> assertThat(tx.alias().name()).isEqualTo(alias),
+                () -> assertThat(tx.sender()).isEqualTo(alice.publicKey()),
+                () -> assertThat(tx.type()).isEqualTo(10),
+                () -> assertThat((Object) txInfo.tx().fee().value()).isEqualTo(MIN_FEE)
+        );
+    }
 }
