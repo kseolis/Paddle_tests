765cbd81f6748abaa865a327ad84f7a32f6e9925
vnikolaenko <vnikolaenko@web3tech.ru>
2022-04-22 15:17:48 +0300

    5 out of 6 tests work

--- src/test/java/im/mak/paddle/e2e/transactions/MassTransferTransactionTest.java
+++ src/test/java/im/mak/paddle/e2e/transactions/MassTransferTransactionTest.java
@@ -3,13 +3,17 @@ package im.mak.paddle.e2e.transactions;
 import com.wavesplatform.transactions.MassTransferTransaction;
 import com.wavesplatform.transactions.common.AssetId;
 import com.wavesplatform.transactions.common.Base58String;
-import com.wavesplatform.transactions.common.Recipient;
 import com.wavesplatform.transactions.mass.Transfer;
 import com.wavesplatform.wavesj.info.TransactionInfo;
 import im.mak.paddle.Account;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import static com.wavesplatform.transactions.common.AssetId.WAVES;
 import static com.wavesplatform.wavesj.ApplicationStatus.SUCCEEDED;
@@ -22,28 +26,12 @@ import static org.junit.jupiter.api.Assertions.assertAll;
 
 public class MassTransferTransactionTest {
     private static Account alice;
-    private static long aliceWavesBalance;
-    private static long aliceAssetBalance;
 
-    private static Account accountName1;
-    private static Recipient accountName1Recipient;
-    private static long accountName1Balance;
-
-    private static Account accountName2;
-    private static Recipient accountName2Recipient;
-    private static long accountName2Balance;
-
-    private static Account accountName3;
-    private static Recipient accountName3Recipient;
-    private static long accountName3Balance;
-
-    private static Account accountName4;
-    private static Recipient accountName4Recipient;
-    private static long accountName4Balance;
-
-    private static Account accountName5;
-    private static Recipient accountName5Recipient;
-    private static long accountName5Balance;
+    private static Account account;
+    private static Account account1;
+    private static Account account2;
+    private static Account account3;
+    private static Account account4;
 
     private static AssetId issuedAsset;
     private static Base58String base58StringAttachment;
@@ -58,33 +46,21 @@ public class MassTransferTransactionTest {
                     alice = new Account(DEFAULT_FAUCET);
                     alice.createAlias(randomNumAndLetterString(15));
                     issuedAsset = alice.issue(i -> i.name("Test_Asset").quantity(1000_00000000L)).tx().assetId();
-                    aliceWavesBalance = alice.getBalance(WAVES);
-                    aliceAssetBalance = alice.getBalance(issuedAsset);
                 },
                 () -> {
-                    accountName1 = new Account();
-                    accountName1Recipient = new Account(DEFAULT_FAUCET).address();
-                    accountName1Balance = accountName1.getWavesBalance();
+                    account = new Account(DEFAULT_FAUCET);
                 },
                 () -> {
-                    accountName2 = new Account();
-                    accountName2Recipient = new Account(DEFAULT_FAUCET).address();
-                    accountName2Balance = accountName2.getWavesBalance();
+                    account1 = new Account(DEFAULT_FAUCET);
                 },
                 () -> {
-                    accountName3 = new Account();
-                    accountName3Recipient = new Account(DEFAULT_FAUCET).address();
-                    accountName3Balance = accountName3.getWavesBalance();
+                    account2 = new Account(DEFAULT_FAUCET);
                 },
                 () -> {
-                    accountName4 = new Account();
-                    accountName4Recipient = new Account(DEFAULT_FAUCET).address();
-                    accountName4Balance = accountName4.getWavesBalance();
-                    },
+                    account3 = new Account(DEFAULT_FAUCET);
+                },
                 () -> {
-                    accountName5 = new Account();
-                    accountName5Recipient = new Account(DEFAULT_FAUCET).address();
-                    accountName5Balance = accountName5.getWavesBalance();
+                    account4 = new Account(DEFAULT_FAUCET);
                 }
         );
     }
@@ -92,33 +68,48 @@ public class MassTransferTransactionTest {
     @Test
     @DisplayName("one transfer in a 'mass transfer transaction'")
     void oneTransferInMassTransfer() {
-        Transfer transfer = Transfer.to(accountName1Recipient, MIN_TRANSFER_SUM);
-        massTransferTransaction(WAVES, MIN_TRANSFER_SUM, transfer);
+        massTransferTransaction(WAVES, MIN_TRANSFER_SUM, account);
     }
 
     @Test
-    @DisplayName("transfer in a 'mass transfer transaction' for four recipients")
-    void massTransferFourRecipients() {
-        Transfer transfer = Transfer.to(accountName1Recipient, MIN_TRANSFER_SUM);
-        Transfer transfer2 = Transfer.to(accountName2Recipient, MIN_TRANSFER_SUM);
-        Transfer transfer3 = Transfer.to(accountName3Recipient, MIN_TRANSFER_SUM);
-        Transfer transfer4 = Transfer.to(accountName4Recipient, MIN_TRANSFER_SUM);
-        massTransferTransaction(WAVES, MIN_TRANSFER_SUM, transfer, transfer2, transfer3, transfer4);
+    @DisplayName("transfer in a 'mass transfer transaction' for three Accounts")
+    void massTransferThreeAccounts() {
+        massTransferTransaction(WAVES, ONE_WAVES, account, account1, account2);
     }
 
     @Test
-    @DisplayName("transfer in a 'mass transfer transaction' for five recipients")
-    void massTransfer() {
-        Transfer transfer = Transfer.to(accountName1Recipient, MIN_TRANSFER_SUM);
-        Transfer transfer2 = Transfer.to(accountName2Recipient, MIN_TRANSFER_SUM);
-        Transfer transfer3 = Transfer.to(accountName3Recipient, MIN_TRANSFER_SUM);
-        Transfer transfer4 = Transfer.to(accountName4Recipient, MIN_TRANSFER_SUM);
-        Transfer transfer5 = Transfer.to(accountName5Recipient, MIN_TRANSFER_SUM);
-        massTransferTransaction(WAVES, MIN_TRANSFER_SUM, transfer, transfer2, transfer3, transfer4, transfer5);
+    @DisplayName("transfer in a 'mass transfer transaction' for five Accounts")
+    void massTransferFiveAccounts() {
+        massTransferTransaction(WAVES, MIN_TRANSFER_SUM, account, account1, account2, account3, account4);
     }
 
-    private void massTransferTransaction(AssetId assetId, long amount, Transfer... transfers) {
-        long balanceAfterMassTransfer = balanceAfterMassTransferCalculation(assetId, amount, transfers.length);
+    @Test
+    @DisplayName("one transfer in a 'mass transfer transaction' issued asset")
+    void oneTransferInMassTransferForIssueAsset() {
+        massTransferTransaction(issuedAsset, MIN_TRANSFER_SUM, account);
+    }
+
+    @Test
+    @DisplayName("transfer in a 'mass transfer transaction' issued asset for three Accounts")
+    void massTransferThreeAccountsForIssueAsset() {
+        massTransferTransaction(issuedAsset, ONE_WAVES, account, account1, account2);
+    }
+
+    @Test
+    @DisplayName("transfer in a 'mass transfer transaction' issued asset for five Accounts")
+    void massTransferFiveAccountsForIssueAsset() {
+        massTransferTransaction(issuedAsset, MIN_TRANSFER_SUM, account, account1, account2, account3, account4);
+    }
+
+    private void massTransferTransaction(AssetId assetId, long amount, Account... accounts) {
+        List<Account> accountsList = Arrays.asList(accounts);
+        List<Transfer> transfers = new ArrayList<>();
+        accountsList.forEach(a -> transfers.add(Transfer.to(a.address(), amount)));
+        long recipientBalanceAfterTransaction = account.getBalance(assetId) + amount;
+
+        int numberOfAccounts = accountsList.size();
+
+        long balanceAfterMassTransfer = balanceAfterMassTransferCalculation(assetId, amount, numberOfAccounts);
 
         MassTransferTransaction tx = alice.massTransfer(i -> i
                 .attachment(base58StringAttachment)
@@ -131,31 +122,38 @@ public class MassTransferTransactionTest {
         assertAll(
                 () -> assertThat(alice.getBalance(assetId)).isEqualTo(balanceAfterMassTransfer),
                 () -> assertThat(txInfo.applicationStatus()).isEqualTo(SUCCEEDED),
+                () -> assertThat(tx.attachment()).isEqualTo(base58StringAttachment),
                 () -> assertThat(tx.sender()).isEqualTo(alice.publicKey()),
+                () -> assertThat(tx.transfers().size()).isEqualTo(numberOfAccounts),
+                () -> tx.transfers().forEach(transfer -> assertThat(transfer.amount()).isEqualTo(amount)),
                 () -> assertThat(tx.type()).isEqualTo(11),
-                () -> assertThat((Object) txInfo.tx().fee().value()).isEqualTo(transactionCommission)
+                () -> assertThat((Object) txInfo.tx().fee().value()).isEqualTo(transactionCommission),
+                () -> accountsList.forEach(
+                        account -> System.out.println(
+                                "account balance: " + account.getBalance(assetId) + "\n" +
+                                "recipientBalanceAfterTransaction: " + recipientBalanceAfterTransaction
+                        ) // assertThat(account.getBalance(assetId)).isEqualTo(recipientBalanceAfterTransaction)
+                )
         );
     }
 
-    private long balanceAfterMassTransferCalculation(AssetId assetId, long amount, int numberOfRecipients) {
+    private long balanceAfterMassTransferCalculation(AssetId assetId, long amount, int numberOfAccounts) {
+        long senderBalance = alice.getBalance(assetId);
         long numForRoundCheck = 100000;
-        long additionalFeeForMassTransfer = FEE_FOR_MASS_TRANSFER * numberOfRecipients;
-        if (additionalFeeForMassTransfer % numForRoundCheck != 0) {
-            additionalFeeForMassTransfer = (long) Math.ceil((float) additionalFeeForMassTransfer / numForRoundCheck) * numForRoundCheck;
+        long additionalFeeForMassTransfer = FEE_FOR_MASS_TRANSFER * numberOfAccounts;
+        if (additionalFeeForMassTransfer % numForRoundCheck != 0) { // The fee value is rounded up to three decimals.
+            additionalFeeForMassTransfer = (long) Math.ceil(
+                    (float) additionalFeeForMassTransfer / numForRoundCheck
+            ) * numForRoundCheck;
         }
 
         transactionCommission = MIN_FEE + additionalFeeForMassTransfer;
-        long transactionSum = amount * numberOfRecipients;
-        long balanceAfterMassTransfer = alice.getBalance(assetId) - transactionCommission - transactionSum;
-        long result = alice.getBalance(assetId) - transactionCommission - transactionSum;
+        long transactionSum = amount * numberOfAccounts;
 
-        System.out.println("numberOfRecipients: " + numberOfRecipients);
-        System.out.println("additionalFeeForMassTransfer: " + additionalFeeForMassTransfer);
-        System.out.println("transactionCommission: " + transactionCommission);
-        System.out.println("transactionSum: " + transactionSum);
-        System.out.println("balanceAfterMassTransfer: " + balanceAfterMassTransfer);
-
-        return result;
+        if(assetId.equals(WAVES)) {
+            return senderBalance - transactionCommission - transactionSum;
+        }
+        return senderBalance - transactionSum;
     }
 }
 
