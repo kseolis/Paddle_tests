f99c6c9c10e9f9f5bbf7bf52a5c432b4a5d96112
vnikolaenko <vnikolaenko@web3tech.ru>
2022-05-04 17:07:42 +0300

    added test "invoke script with small complexity" for InvokeScriptTransactionTest.java

--- src/test/java/im/mak/paddle/e2e/transactions/InvokeScriptTransactionTest.java
+++ src/test/java/im/mak/paddle/e2e/transactions/InvokeScriptTransactionTest.java
@@ -1,4 +1,55 @@
 package im.mak.paddle.e2e.transactions;
 
+import com.wavesplatform.transactions.InvokeScriptTransaction;
+import com.wavesplatform.transactions.common.Amount;
+import com.wavesplatform.wavesj.info.TransactionInfo;
+import im.mak.paddle.Account;
+import im.mak.paddle.dapp.DAppCall;
+import im.mak.paddle.dapps.IntDApp;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static com.wavesplatform.wavesj.ApplicationStatus.SUCCEEDED;
+import static im.mak.paddle.Node.node;
+import static im.mak.paddle.helpers.Randomizer.getRandomInt;
+import static im.mak.paddle.token.Waves.WAVES;
+import static im.mak.paddle.util.Constants.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
 public class InvokeScriptTransactionTest {
+    private static IntDApp accWithDApp;
+    private static Account account;
+
+    @BeforeAll
+    static void before() {
+        accWithDApp = new IntDApp(DEFAULT_FAUCET);
+        account = new Account(DEFAULT_FAUCET);
+    }
+
+    @Test
+    @DisplayName("invoke script with small complexity")
+    void invokeTest() {
+        Amount amount = WAVES.of(0.1);
+        invokeTransaction(account, accWithDApp.setInt(getRandomInt(1, 50)), amount);
+    }
+
+    private void invokeTransaction(Account callingAcc, DAppCall dAppCall, Amount amount) {
+        long minFee = MIN_FEE + EXTRA_FEE;
+        long balanceAfterTransaction = callingAcc.getWavesBalance() - minFee - amount.value();
+
+        InvokeScriptTransaction tx = callingAcc.invoke(dAppCall, amount).tx();
+        TransactionInfo txInfo = node().getTransactionInfo(tx.id());
+
+        assertAll(
+                () -> assertThat(txInfo.applicationStatus()).isEqualTo(SUCCEEDED),
+                () -> assertThat(tx.dApp()).isEqualTo(dAppCall.getDApp()),
+                () -> assertThat(tx.function()).isEqualTo(dAppCall.getFunction()),
+                () -> assertThat(tx.sender()).isEqualTo(callingAcc.publicKey()),
+                () -> assertThat(tx.type()).isEqualTo(16),
+                () -> assertThat(tx.fee().value()).isEqualTo(minFee),
+                () -> assertThat(callingAcc.getWavesBalance()).isEqualTo(balanceAfterTransaction)
+        );
+    }
 }
