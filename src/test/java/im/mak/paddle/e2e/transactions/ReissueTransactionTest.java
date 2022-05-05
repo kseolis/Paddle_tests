654ac55480df38c9d0064304b6686653536a17e8
vnikolaenko <vnikolaenko@web3tech.ru>
2022-05-05 18:56:35 +0300

    added asserts for ReissueTransactionTest

--- src/test/java/im/mak/paddle/e2e/transactions/ReissueTransactionTest.java
+++ src/test/java/im/mak/paddle/e2e/transactions/ReissueTransactionTest.java
@@ -17,7 +17,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 public class ReissueTransactionTest {
-    private static Account alice;
+    private static Account account;
     private static long aliceWavesBalance;
     private static AssetId issuedAssetId;
     private static AssetId issuedSmartAssetId;
@@ -25,56 +25,58 @@ public class ReissueTransactionTest {
 
     @BeforeAll
     static void before() {
-        alice = new Account(DEFAULT_FAUCET);
+        account = new Account(DEFAULT_FAUCET);
         async(
-            () -> alice.createAlias(randomNumAndLetterString(15)),
-            () -> issuedAssetId = alice.issue(i -> i.name("Test_Asset").quantity(1000)).tx().assetId(),
-            () -> issuedSmartAssetId =
-                    alice.issue(i -> i.name("Test_Asset").quantity(1000).script("2 * 2 == 4")).tx().assetId()
+                () -> account.createAlias(randomNumAndLetterString(15)),
+                () -> issuedAssetId = account.issue(i -> i.name("Test_Asset").quantity(1000)).tx().assetId(),
+                () -> issuedSmartAssetId =
+                        account.issue(i -> i.name("T_Smart_Asset").quantity(1000).script("2 * 2 == 4")).tx().assetId()
         );
     }
 
     @Test
-    @DisplayName("test reissue minimum quantity asset")
+    @DisplayName("reissue minimum quantity asset")
     void reissueMinimumAssets() {
         reissueTransaction(ASSET_QUANTITY_MINIMUM, issuedAssetId, MIN_FEE);
     }
 
     @Test
-    @DisplayName("test reissue maximum quantity asset")
+    @DisplayName("reissue maximum quantity asset")
     void reissueAlmostMaximumAssets() {
-        long reissueSum = ASSET_QUANTITY_MAXIMUM - alice.getAssetBalance(issuedAssetId) - ASSET_QUANTITY_MINIMUM;
+        long reissueSum = ASSET_QUANTITY_MAXIMUM - account.getAssetBalance(issuedAssetId) - ASSET_QUANTITY_MINIMUM;
         reissueTransaction(reissueSum, issuedAssetId, MIN_FEE);
     }
 
     @Test
-    @DisplayName("test reissue minimum quantity smart asset")
+    @DisplayName("reissue minimum quantity smart asset")
     void reissueMinimumSmartAssets() {
         reissueTransaction(ASSET_QUANTITY_MINIMUM, issuedSmartAssetId, feeForSmartAssetReissue);
     }
 
     @Test
-    @DisplayName("test reissue maximum quantity smart asset")
+    @DisplayName("reissue maximum quantity smart asset")
     void reissueAlmostMaximumSmartAssets() {
-        long reissueSum = ASSET_QUANTITY_MAXIMUM - alice.getAssetBalance(issuedSmartAssetId) - ASSET_QUANTITY_MINIMUM;
+        long reissueSum = ASSET_QUANTITY_MAXIMUM - account.getAssetBalance(issuedSmartAssetId) - ASSET_QUANTITY_MINIMUM;
         reissueTransaction(reissueSum, issuedSmartAssetId, feeForSmartAssetReissue);
     }
 
     private void reissueTransaction(long amount, AssetId assetId, long fee) {
-        aliceWavesBalance = alice.getBalance(AssetId.WAVES);
-        long balanceAfterReissue = alice.getBalance(assetId) + amount;
-        ReissueTransaction tx = alice.reissue(amount, assetId, i -> i.reissuable(true)).tx();
+        aliceWavesBalance = account.getBalance(AssetId.WAVES);
+        long balanceAfterReissue = account.getBalance(assetId) + amount;
+        ReissueTransaction tx = account.reissue(amount, assetId, i -> i.reissuable(true)).tx();
 
         TransactionInfo txInfo = node().getTransactionInfo(tx.id());
 
         assertAll(
-                () -> assertThat(alice.getAssetBalance(assetId)).isEqualTo(balanceAfterReissue),
-                () -> assertThat(alice.getWavesBalance()).isEqualTo(aliceWavesBalance - fee),
                 () -> assertThat(txInfo.applicationStatus()).isEqualTo(SUCCEEDED),
-                () -> assertThat(tx.sender()).isEqualTo(alice.publicKey()),
+                () -> assertThat(account.getAssetBalance(assetId)).isEqualTo(balanceAfterReissue),
+                () -> assertThat(account.getWavesBalance()).isEqualTo(aliceWavesBalance - fee),
+                () -> assertThat(tx.fee().value()).isEqualTo(fee),
+                () -> assertThat(tx.sender()).isEqualTo(account.publicKey()),
                 () -> assertThat(tx.reissuable()).isEqualTo(true),
-                () -> assertThat(tx.type()).isEqualTo(5),
-                () -> assertThat((Object) txInfo.tx().fee().value()).isEqualTo(fee)
+                () -> assertThat(tx.amount().value()).isEqualTo(amount),
+                () -> assertThat(tx.amount().assetId()).isEqualTo(assetId),
+                () -> assertThat(tx.type()).isEqualTo(5)
         );
     }
 }
