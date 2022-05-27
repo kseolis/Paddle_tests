0923ee1621d87e04fb7b8fef64c6e46cbb7e9821
vnikolaenko <vnikolaenko@web3tech.ru>
2022-05-27 16:16:14 +0300

    SubscribeTest.java

--- src/test/java/im/mak/paddle/blockchain_updates/SubscribeTest.java
+++ src/test/java/im/mak/paddle/blockchain_updates/SubscribeTest.java
@@ -1,36 +1,65 @@
 package im.mak.paddle.blockchain_updates;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.googlecode.protobuf.format.JsonFormat;
+import com.wavesplatform.crypto.base.Base64;
 import com.wavesplatform.events.api.grpc.protobuf.BlockchainUpdates;
 import com.wavesplatform.events.api.grpc.protobuf.BlockchainUpdatesApiGrpc;
+import com.wavesplatform.transactions.common.Base64String;
+import im.mak.paddle.blockchain_updates.serialized_json.Update;
 import io.grpc.StatusRuntimeException;
 import org.junit.jupiter.api.Test;
+import org.jvnet.hk2.internal.ClazzCreator;
 
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
+import java.util.regex.Pattern;
 
-import static com.google.common.truth.Truth.assertThat;
 import static com.wavesplatform.events.api.grpc.protobuf.BlockchainUpdatesApiGrpc.newBlockingStub;
+import static im.mak.paddle.helpers.Randomizer.randomNumAndLetterString;
 
 public class SubscribeTest extends BaseTest {
-
     @Test
-    void subscribeTest() {
-        int fromHeight = getHeight() - 10;
+    void subscribeTestForCreateAlias() {
+        String newAlias = randomNumAndLetterString(16);
+        account.createAlias(newAlias);
+        checkSubscribeResponse();
+    }
 
-        BlockchainUpdates.SubscribeRequest subscribeRequest = BlockchainUpdates.SubscribeRequest
+    private void checkSubscribeResponse() {
+        BlockchainUpdates.SubscribeRequest request = BlockchainUpdates.SubscribeRequest
                 .newBuilder()
-                .setFromHeight(fromHeight)
+                .setFromHeight(getHeight() - 1)
                 .setToHeight(getHeight())
                 .build();
 
         BlockchainUpdatesApiGrpc.BlockchainUpdatesApiBlockingStub stub = newBlockingStub(channel);
+        Iterator<BlockchainUpdates.SubscribeEvent> subscribe = stub.subscribe(request);
 
-        Iterator<BlockchainUpdates.SubscribeEvent> subscribe = stub.subscribe(subscribeRequest);
+        checkJsonSchema(subscribe);
+    }
 
+
+    private void checkJsonSchema(Iterator<BlockchainUpdates.SubscribeEvent> subscribe) {
+        String response;
+        Update deserialized;
         try {
             while (subscribe.hasNext()) {
-                System.out.println(subscribe.next());
-                assertThat(subscribe).isNotNull();
+                response = new JsonFormat().printToString(subscribe.next())
+                        .replaceAll("\\\\[va]", "±");
+                System.out.println(response);
+                deserialized = deserialize(response, Update.class);
+                System.out.println(deserialized);
             }
-        } catch (StatusRuntimeException ignored) {}
+        } catch (StatusRuntimeException ignored) {
+        }
+    }
+
+    public <T> T deserialize(String jsonString, Class<T> clazz) {
+        GsonBuilder builder = new GsonBuilder();
+        Gson gson = builder.create();
+        return gson.fromJson(jsonString, clazz);
     }
 }
