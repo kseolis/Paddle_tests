1eeed2879aeaceea9c3638af855181807cd2a79d
vnikolaenko <vnikolaenko@web3tech.ru>
2022-07-05 15:05:40 +0300

    edited data transaction subscribe test

--- src/test/java/im/mak/paddle/blockchain_updates/subscribe_tests/DataTransactionSubscriptionTest.java
+++ src/test/java/im/mak/paddle/blockchain_updates/subscribe_tests/DataTransactionSubscriptionTest.java
@@ -2,16 +2,15 @@ package im.mak.paddle.blockchain_updates.subscribe_tests;
 
 import com.wavesplatform.transactions.DataTransaction;
 import com.wavesplatform.transactions.common.Base64String;
-import com.wavesplatform.transactions.data.BinaryEntry;
-import com.wavesplatform.transactions.data.BooleanEntry;
-import com.wavesplatform.transactions.data.IntegerEntry;
-import com.wavesplatform.transactions.data.StringEntry;
+import com.wavesplatform.transactions.data.*;
 import im.mak.paddle.Account;
 import im.mak.paddle.blockchain_updates.BaseTest;
+import im.mak.paddle.helpers.blockchain_updates_handlers.subscribe_handlers.transaction_state_updates.DataEntries;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static com.wavesplatform.transactions.DataTransaction.LATEST_VERSION;
 import static im.mak.paddle.Node.node;
 import static im.mak.paddle.helpers.Randomizer.getRandomInt;
 import static im.mak.paddle.helpers.Randomizer.randomNumAndLetterString;
@@ -23,7 +22,7 @@ import static im.mak.paddle.helpers.blockchain_updates_handlers.subscribe_handle
 import static im.mak.paddle.helpers.blockchain_updates_handlers.subscribe_handlers.transactions_handlers.TransactionsHandler.*;
 import static im.mak.paddle.helpers.blockchain_updates_handlers.subscribe_handlers.transactions_handlers.TransactionsHandler.getTransactionVersion;
 import static im.mak.paddle.helpers.transaction_senders.BaseTransactionSender.getTxInfo;
-import static im.mak.paddle.helpers.transaction_senders.DataTransactionsSender.dataEntryTransactionSender;
+import static im.mak.paddle.helpers.transaction_senders.DataTransactionsSender.*;
 import static im.mak.paddle.util.Async.async;
 import static im.mak.paddle.util.Constants.*;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -33,12 +32,7 @@ public class DataTransactionSubscriptionTest extends BaseTest {
     private String senderAddress;
     private String senderPublicKey;
     private Account senderAccount;
-
-    private final long wavesAmountBeforeBurn = DEFAULT_FAUCET - ONE_WAVES;
-    private final byte[] compileScript = node().compileScript(SCRIPT_PERMITTING_OPERATIONS).script().bytes();
-
     private final Base64String base64String = new Base64String(randomNumAndLetterString(6));
-
     private final BinaryEntry binaryEntry = BinaryEntry.as("BinEntry", base64String);
     private final BooleanEntry booleanEntry = BooleanEntry.as("Boolean", true);
     private final IntegerEntry integerEntry = IntegerEntry.as("Integer", getRandomInt(100, 100000));
@@ -58,15 +52,24 @@ public class DataTransactionSubscriptionTest extends BaseTest {
     @Test
     @DisplayName("Check subscription on data smart asset transaction")
     void subscribeTestForDataTransaction() {
-        dataEntryTransactionSender(senderAccount, DataTransaction.LATEST_VERSION, integerEntry);
+        dataEntryTransactionSender(senderAccount,
+                LATEST_VERSION,
+                integerEntry,
+                binaryEntry,
+                booleanEntry,
+                stringEntry);
         height = node().getHeight();
         subscribeResponseHandler(channel, senderAccount, height, height);
+        checkDataTransactionSubscribe();
+        checkDataEntries();
+    }
 
+    private void checkDataTransactionSubscribe() {
         assertAll(
                 () -> assertThat(getChainId(0)).isEqualTo(DEVNET_CHAIN_ID),
                 () -> assertThat(getSenderPublicKeyFromTransaction(0)).isEqualTo(senderPublicKey),
                 () -> assertThat(getTransactionFeeAmount(0)).isEqualTo(MIN_FEE),
-                () -> assertThat(getTransactionVersion(0)).isEqualTo(DataTransaction.LATEST_VERSION),
+                () -> assertThat(getTransactionVersion(0)).isEqualTo(LATEST_VERSION),
                 () -> assertThat(getTransactionId()).isEqualTo(getTxInfo().tx().id().toString()),
 
                 () -> assertThat(getKeyFromDataTx(0, 0)).isEqualTo(integerEntry.key()),
@@ -74,15 +77,38 @@ public class DataTransactionSubscriptionTest extends BaseTest {
                 // check waves balance
                 () -> assertThat(getAddress(0, 0)).isEqualTo(senderAddress),
                 () -> assertThat(getAmountBefore(0, 0)).isEqualTo(DEFAULT_FAUCET),
-                () -> assertThat(getAmountAfter(0, 0)).isEqualTo(DEFAULT_FAUCET - MIN_FEE),
+                () -> assertThat(getAmountAfter(0, 0)).isEqualTo(DEFAULT_FAUCET - MIN_FEE)
+        );
+    }
 
-                // before data transaction
-                () -> assertThat(getBeforeDataEntries(0, 0).getKey()).isEqualTo(integerEntry.key()),
-                () -> assertThat(getBeforeDataEntries(0, 0).getIntValue()).isEqualTo(0),
-                // after data transaction
-                () -> assertThat(getSenderAddress(0, 0)).isEqualTo(senderAddress),
-                () -> assertThat(getTxKeyForStateUpdates(0, 0)).isEqualTo(integerEntry.key()),
-                () -> assertThat(getTxIntValueForStateUpdates(0,0)).isEqualTo(integerEntry.value())
+    private void checkDataEntries() {
+        assertAll(
+                () -> getDataTx().data().forEach(
+                        data -> assertThat
+                                (getBeforeDataEntries(0, getDataTx().data().indexOf(data)).getKey())
+                                .isEqualTo(data.key())
+                ),
+                () -> getDataTx().data().forEach(
+                        data -> assertThat(
+                                (getBeforeDataEntries(0, getDataTx().data().indexOf(data))
+                                        .getValueCase()
+                                        .toString()))
+                                .isEqualTo("VALUE_NOT_SET")
+                ),
+                () -> getDataTx().data().forEach(
+                        data -> assertThat(
+                                (getSenderAddress(0, getDataTx().data().indexOf(data))))
+                                .isEqualTo(senderAddress)
+                ),
+                () -> getDataTx().data().forEach(
+                        data -> assertThat(
+                                (getTxKeyForStateUpdates(0, getDataTx().data().indexOf(data))))
+                                .isEqualTo(data.key())
+                ),
+                () -> assertThat(getTxIntValueForStateUpdates(0, 0)).isEqualTo(integerEntry.value()),
+                () -> assertThat(getTxByteValueForStateUpdates(0, 1)).isEqualTo(binaryEntry.value().toString()),
+                () -> assertThat(getTxBoolValueForStateUpdates(0, 2)).isEqualTo(booleanEntry.value()),
+                () -> assertThat(getTxStringValueForStateUpdates(0, 3)).isEqualTo(stringEntry.value())
         );
     }
 }
--- src/test/java/im/mak/paddle/helpers/blockchain_updates_handlers/subscribe_handlers/transaction_state_updates/DataEntries.java
+++ src/test/java/im/mak/paddle/helpers/blockchain_updates_handlers/subscribe_handlers/transaction_state_updates/DataEntries.java
@@ -1,6 +1,7 @@
 package im.mak.paddle.helpers.blockchain_updates_handlers.subscribe_handlers.transaction_state_updates;
 
 import com.wavesplatform.crypto.base.Base58;
+import com.wavesplatform.crypto.base.Base64;
 import com.wavesplatform.events.protobuf.Events;
 import com.wavesplatform.protobuf.transaction.TransactionOuterClass;
 
@@ -17,6 +18,18 @@ public class DataEntries {
 
     public static long getTxIntValueForStateUpdates(int txStateUpdIndex, int index) {
         return getDataEntries(txStateUpdIndex, index).getDataEntry().getIntValue();
+    }
+
+    public static String getTxStringValueForStateUpdates(int txStateUpdIndex, int index) {
+        return getDataEntries(txStateUpdIndex, index).getDataEntry().getStringValue();
+    }
+
+    public static boolean getTxBoolValueForStateUpdates(int txStateUpdIndex, int index) {
+        return getDataEntries(txStateUpdIndex, index).getDataEntry().getBoolValue();
+    }
+
+    public static String getTxByteValueForStateUpdates(int txStateUpdIndex, int index) {
+        return Base64.encode(getDataEntries(txStateUpdIndex, index).getDataEntry().getBinaryValue().toByteArray());
     }
 
     public static long getBeforeDataEntriesIntegerValue(int txStateUpdIndex, int index) {
