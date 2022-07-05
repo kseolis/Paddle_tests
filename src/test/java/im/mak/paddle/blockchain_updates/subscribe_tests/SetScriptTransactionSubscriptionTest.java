7e990b4ed17af13fa928a8243b24eebe40e5b098
vnikolaenko <vnikolaenko@web3tech.ru>
2022-07-05 15:27:19 +0300

    added tests for SetScriptTransactionSubscriptionTest.java

--- src/test/java/im/mak/paddle/blockchain_updates/subscribe_tests/SetScriptTransactionSubscriptionTest.java
+++ src/test/java/im/mak/paddle/blockchain_updates/subscribe_tests/SetScriptTransactionSubscriptionTest.java
@@ -1,6 +1,5 @@
 package im.mak.paddle.blockchain_updates.subscribe_tests;
 
-import com.wavesplatform.transactions.SetScriptTransaction;
 import com.wavesplatform.transactions.common.Base64String;
 import im.mak.paddle.Account;
 import im.mak.paddle.blockchain_updates.BaseTest;
@@ -8,6 +7,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static com.wavesplatform.transactions.SetScriptTransaction.LATEST_VERSION;
 import static im.mak.paddle.Node.node;
 import static im.mak.paddle.helpers.blockchain_updates_handlers.subscribe_handlers.SubscribeHandler.*;
 import static im.mak.paddle.helpers.blockchain_updates_handlers.subscribe_handlers.transaction_state_updates.Balances.*;
@@ -29,7 +29,7 @@ public class SetScriptTransactionSubscriptionTest extends BaseTest {
 
     private long wavesAmountAfterSetAssetScript;
 
-    private final Base64String script = node()
+    private Base64String script = node()
             .compileScript(fromFile("ride_scripts/permissionOnUpdatingKeyValues.ride")).script();
 
     @BeforeEach
@@ -42,16 +42,58 @@ public class SetScriptTransactionSubscriptionTest extends BaseTest {
     @Test
     @DisplayName("Check subscription on setScript transaction")
     void subscribeTestForSetScriptTransaction() {
-        setScriptTransactionSender(account, script, MIN_FEE, SetScriptTransaction.LATEST_VERSION);
+        setScriptTransactionSender(account, script, MIN_FEE, LATEST_VERSION);
         wavesAmountAfterSetAssetScript = DEFAULT_FAUCET - getFee();
         height = node().getHeight();
         subscribeResponseHandler(channel, account, height, height);
+        checkSetScriptSubscribe();
+    }
 
+    @Test
+    @DisplayName("Check subscription on set 32kb_size Script transaction")
+    void subscribeTestForSet32kbScript() {
+        long minimalValSetScriptFee = 2200000;
+        script = node().compileScript(fromFile("ride_scripts/scriptSize32kb.ride")).script();
+        setScriptTransactionSender(account, script, minimalValSetScriptFee, LATEST_VERSION);
+        wavesAmountAfterSetAssetScript = DEFAULT_FAUCET - getFee();
+        height = node().getHeight();
+        subscribeResponseHandler(channel, account, height, height);
+        checkSetScriptSubscribe();
+    }
+
+    @Test
+    @DisplayName("Check subscription on set DApp Script transaction")
+    void subscribeTestForSetDAppScript() {
+        script = node().compileScript("{-# STDLIB_VERSION 4 #-}\n" +
+                "{-# SCRIPT_TYPE ACCOUNT #-}\n" +
+                "{-# CONTENT_TYPE DAPP #-}").script();
+        setScriptTransactionSender(account, script, 0, LATEST_VERSION);
+        wavesAmountAfterSetAssetScript = DEFAULT_FAUCET - getFee();
+        height = node().getHeight();
+        subscribeResponseHandler(channel, account, height, height);
+        checkSetScriptSubscribe();
+    }
+
+    @Test
+    @DisplayName("Check subscription on set LIBRARY Script transaction")
+    void subscribeTestForSetLibraryScript() {
+        script = node().compileScript("{-# STDLIB_VERSION 6 #-}\n" +
+                "{-# SCRIPT_TYPE ACCOUNT #-}\n" +
+                "{-# CONTENT_TYPE LIBRARY #-}").script();
+        setScriptTransactionSender(account, script, 0, LATEST_VERSION);
+        wavesAmountAfterSetAssetScript = DEFAULT_FAUCET - getFee();
+        height = node().getHeight();
+        subscribeResponseHandler(channel, account, height, height);
+        checkSetScriptSubscribe();
+    }
+
+    private void checkSetScriptSubscribe() {
+        System.out.println(getAppend());
         assertAll(
                 () -> assertThat(getChainId(0)).isEqualTo(DEVNET_CHAIN_ID),
                 () -> assertThat(getSenderPublicKeyFromTransaction(0)).isEqualTo(publicKey),
                 () -> assertThat(getTransactionFeeAmount(0)).isEqualTo(getFee()),
-                () -> assertThat(getTransactionVersion(0)).isEqualTo(SetScriptTransaction.LATEST_VERSION),
+                () -> assertThat(getTransactionVersion(0)).isEqualTo(LATEST_VERSION),
                 () -> assertThat(getScriptFromSetScript(0)).isEqualTo(script.bytes()),
                 () -> assertThat(getTransactionId()).isEqualTo(getSetScriptTx().id().toString()),
                 // check waves balance
